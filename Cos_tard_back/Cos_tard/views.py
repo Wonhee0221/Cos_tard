1f3bd8c41ec377cf512758f2fcf8eb950b88f469
rkalsrnjs <gmkwon0812@g.skku.edu>
2023-08-02 15:05:45 +0900

    2차 연결

--- Cos_tard_back/Cos_tard/views.py
+++ Cos_tard_back/Cos_tard/views.py
@@ -2,4 +2,4 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request,'Cos_tard/home_page.html')
+    return render(request,'Cos_tard/home.html')
--- Cos_tard_back/recommend/urls.py
+++ Cos_tard_back/recommend/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'recommend'
 
 urlpatterns = [
-    path('',views.index,name='recommend-page'),
+    path('',views.index,name='recommend-page')
 ]
