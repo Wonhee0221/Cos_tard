655e1b0e5f248087e98bec913b981f9323c75e37
rkalsrnjs <gmkwon0812@g.skku.edu>
2023-08-07 13:53:26 +0900

    .

--- Cos_tard_web/InfluencerList/models.py
+++ Cos_tard_web/InfluencerList/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Users_fix(models.Model):
-    ig_id=models.BigIntegerField(primary_key=True, null=False, unique=True)
+    ig_id=models.BigIntegerField(primary_key=True, null=False)
     user_id=models.CharField()
     username=models.CharField()
-    website=models.TextField()
+    website=models.URLField(null=True)
     biography=models.TextField()
 
     class Meta:
@@ -14,8 +14,7 @@ class Users_fix(models.Model):
         db_table = "Users_fix"
 
 class Users_info(models.Model):
-    key=models.AutoField(primary_key=True)
-    ig_id=models.ForeignKey(Users_fix, on_delete=models.CASCADE)
+    ig_id=models.ForeignKey(Users_fix,on_delete=models.CASCADE)
     date=models.DateField()
     follows_count=models.IntegerField()
     followers_count=models.IntegerField()
@@ -26,11 +25,11 @@ class Users_info(models.Model):
         db_table = "Users_info"
 
 class Media_fix(models.Model):
-    ig_id=models.ForeignKey(Users_fix, on_delete=models.CASCADE)
-    media_id=models.IntegerField(primary_key=True, null=False, unique=True)
-    caption=models.TextField()
-    media_url=models.TextField()
-    permalink=models.TextField()
+    ig_id=models.ForeignKey(Users_fix,on_delete=models.CASCADE)
+    media_id=models.TextField(primary_key=True)
+    caption=models.TextField(null=True)
+    media_url=models.URLField()
+    permalink=models.URLField()
     timestamp=models.DateTimeField()
 
     class Meta:
@@ -38,8 +37,7 @@ class Media_fix(models.Model):
         db_table = "Media_fix"
 
 class Media_info(models.Model):
-    key=models.AutoField(primary_key=True)
-    media_id=models.ForeignKey(Media_fix, on_delete=models.CASCADE)
+    media_id=models.ForeignKey(Media_fix,on_delete=models.CASCADE)
     date=models.DateField()
     like_count=models.IntegerField(null=True)
     comments_count=models.IntegerField(null=True)
