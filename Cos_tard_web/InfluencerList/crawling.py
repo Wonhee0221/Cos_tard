6c073bf91ba44f37fe8275aa8a83b052579dd338
rkalsrnjs <gmkwon0812@g.skku.edu>
2023-08-07 11:25:41 +0900

    .

--- Cos_tard_web/InfluencerList/crawling.py
+++ Cos_tard_web/InfluencerList/crawling.py
@@ -1,6 +1,7 @@
 import requests
 import unicodedata
 import pandas as pd
+from InfluencerList.models import *
 
 """Users_fix테이블"""
 
@@ -137,3 +138,27 @@ def crawl_media_info(admin_id, token, username):
     except requests.exceptions.RequestException as e:
         print(f"에러: {e}")
         return None, None
+    
+
+
+
+##저장 시작##
+admin_id = 17841402050732962
+token = "EAAIvJjhX7PgBO9abNiUoqdZBEc9cSUjW1J9Up1ZCMXiBlJNrmYL4rEiZAjPXHKpZCuZAzE9okSwYUgyTAOYKcdI5iTNv0nD7vq5jVXEIO37dtm0YccPtlGdIozP7A0VeVQ6FZCjysZCdETKBqVVqLP0fiLqzpFthXauUsMj8bgJZBTSdG0BDjTLIBW3BToEwbkwZD"
+username="rkasl_"
+
+# users_fix = crawl_users_fix(admin_id, token, username)
+# users_info = crawl_users_info(admin_id, token, username)
+# media_fix = crawl_media_fix(admin_id, token, username)
+# media_info = crawl_media_info(admin_id, token, username)
+
+
+# Users_fix.objects.create(ig_id=users_fix[0], user_id=users_fix[1], username= users_fix[2], website=users_fix[3], biography=users_fix[4])
+# Users_info.objects.create(ig_id=users_info[0], date=users_info[1], follows_count= users_info[2], followers_count=users_fix[3], media_count=users_info[4])
+
+# for i in media_fix:
+#     Media_fix.objects.create(ig_id=media_fix[i][0], media_id=media_fix[i][1], caption=media_fix[i][2], media_url=media_fix[i][3], permalink=media_fix[i][4],timestamp=media_fix[i][5])
+ 
+# for i in media_info:
+#     Media_info.objects.create(media_id=media_info[i][0], date=media_info[i][1], like_count=media_info[i][2], commendts_count=media_info[i][3])
+
--- Cos_tard_web/InfluencerList/views.py
+++ Cos_tard_web/InfluencerList/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from InfluencerList.models import *
-from InfluencerList.crawling import *
 
 # Create your views here.
 
@@ -15,16 +13,3 @@ def index(request):
 }
 
     return render(request,'InfluencerList/influencerList.html',context=influencer_dic)
-
-admin_id = 17841402050732962
-token = "EAAIvJjhX7PgBO9abNiUoqdZBEc9cSUjW1J9Up1ZCMXiBlJNrmYL4rEiZAjPXHKpZCuZAzE9okSwYUgyTAOYKcdI5iTNv0nD7vq5jVXEIO37dtm0YccPtlGdIozP7A0VeVQ6FZCjysZCdETKBqVVqLP0fiLqzpFthXauUsMj8bgJZBTSdG0BDjTLIBW3BToEwbkwZD"
-username="rkasl_"
-
-users_fix = crawl_users_fix(admin_id, token, username)
-users_fix = crawl_users_info(admin_id, token, username)
-users_fix = crawl_media_fix(admin_id, token, username)
-users_fix = crawl_media_info(admin_id, token, username)
-
-
-#Users_fix.objects.create(ig_id=users_fix[0], user_id=users_fix[1], username= users_fix[2], website=users_fix[3], biography=users_fix[4])
-
