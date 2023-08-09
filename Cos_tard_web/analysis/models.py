a844578aa28868666b835318eed57ba15ef4f0e4
cm2hcm2h <cm2hcm2h@naver.com>
2023-08-09 10:55:33 +0900

    분석페이지(analysis.html) 검색기능 활성화

--- Cos_tard_web/analysis/models.py
+++ Cos_tard_web/analysis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class testTable(models.Model):
+    id=models.CharField(primary_key=True)
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    followerCount=models.IntegerField()
+
+    class Meta:
+        managed=False
+        db_table="testTable"
--- Cos_tard_web/analysis/urls.py
+++ Cos_tard_web/analysis/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'analysis'
 
 urlpatterns = [
-    path('',views.index,name='analysis-page')
+    path('',views.index,name='analysis-page'),
+    path('get_influencer',views.get_influencer,name="getinfluencer")
 ]
