e77b652604c6569ae0e230e48c5f98daa3e0ba7b
권가민 <gmkwon0812@g.skku.edu>
2023-08-11 00:34:32 +0900

    influencer list page raw 완료, 크롤링페이지 디자인 조금

--- Cos_tard_web/InfluencerList/views.py
+++ Cos_tard_web/InfluencerList/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from media4.models import Users_fix, Users_info
+from django.db.models import Subquery, OuterRef
 
 # Create your views here.
 
 def index(request): 
 
     selected_users_fix = Users_fix.objects.values('ig_id', 'user_id', 'username')
-    #selected_users_info
+    total_rows = Users_fix.objects.all().count()
+    selected_users_info = Users_info.objects.order_by('-date')[:total_rows].values('ig_id', 'follows_count', 'followers_count', 'media_count', 'date')
 
-    context = {'selected_users_fix': selected_users_fix}
+    joined_query = selected_users_info.annotate(
+        user_id=Subquery(selected_users_fix.filter(ig_id=OuterRef('ig_id')).values('user_id')),
+        username=Subquery(selected_users_fix.filter(ig_id=OuterRef('ig_id')).values('username'))
+        )
+    context = {'joined_query': joined_query}
 
     
     return render(request,'InfluencerList/influencerList.html',context)
