12bb1177169b048fa91944c8b6cb459a40a1bc96
권가민 <gmkwon0812@g.skku.edu>
2023-08-14 02:32:09 +0900

    해시태그 추출 추가

--- Cos_tard_web/analysis/processing.py
+++ Cos_tard_web/analysis/processing.py
@@ -5,11 +5,28 @@ def follower_graph(ig_id):
     follower_trend = list(follower_trend)
     xData = [data['date'] for data in follower_trend]
     yData = [data['followers_count'] for data in follower_trend]
+    growth_rates = [0]
+
+    for i in range(1, len(yData)):
+        prev_value = yData[i - 1]
+        current_value = yData[i]
+        
+        growth_rate = ((current_value - prev_value) / prev_value) * 100
+        growth_rates.append(growth_rate)
+    
+    total_growth = sum (growth_rates)
+    average_growth = total_growth / len(yData)
+    
     
     follower_trend = {
         'xData': xData,
         'yData': yData,
+        'growth_rates' : growth_rates,
+        'average_growth' : average_growth
+
     }
     return (follower_trend)
+
+
 
 
