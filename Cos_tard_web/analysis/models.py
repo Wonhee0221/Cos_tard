a202372b0caa7c009899c4810c6bb7b231b62e71
Wonhee0221 <totohwh3601@naver.com>
2023-08-13 16:34:22 +0900

    검색유저정보표현

--- Cos_tard_web/analysis/models.py
+++ Cos_tard_web/analysis/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-
-# Create your models here.
-class testTable(models.Model):
-    id=models.CharField(primary_key=True)
-    name=models.CharField(max_length=100)
-    age=models.IntegerField()
-    followercount=models.IntegerField()
-
-    class Meta:
-        managed=False
-        db_table="testTable"
--- Cos_tard_web/analysis/views.py
+++ Cos_tard_web/analysis/views.py
@@ -3,11 +3,47 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from analysis.models import *
 import json
+from media4.models import Users_fix, Users_info
+from django.db.models import Subquery, OuterRef
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def index(request): 
-    return render(request,'analysis/analysis.html')
+@csrf_exempt
+def get_influencer_infofix(request):
+    data = json.loads(request.body)
+    influencer_name = data.get('influencer_name')
+    try:
+        influencer = Users_fix.objects.get(user_id=influencer_name)
+        influencer_data = {
+            'userid': influencer.user_id,
+            'username': influencer.username,
+            'website': influencer.website,
+            'biography': influencer.biography,
+        }
+        return JsonResponse(influencer_data, safe=False)
+    except Users_fix.DoesNotExist:
+        return JsonResponse({'error': 'Influencer not found'}, status=404)
+
+@csrf_exempt
+def get_influencer_info(request):
+    data = json.loads(request.body)
+    influencer_name = data.get('influencer_name')
+    try:
+        influencer = Users_fix.objects.get(user_id=influencer_name)
+        influencer_details = list(Users_info.objects.filter(ig_id=influencer.ig_id).order_by('-date')[:1].values())[0]
+        influencer_data = {
+            'follows_count': influencer_details.get('follows_count'),
+            'followers_count': influencer_details.get('followers_count'),
+            'media_count': influencer_details.get('media_count')
+            # 다른 필요한 정보들도 추가할 수 있음
+        }
+        return JsonResponse(influencer_data, safe=False)
+    except Users_fix.DoesNotExist:
+        return JsonResponse({'error': 'Influencer not found'}, status=404)
+    
+
+    
+
 
 
 # DB에서 검색해서 웹페이지에 결과 띄우는거 시험해보는중..
@@ -24,15 +60,3 @@ def index(request):
 #         return render(request, 'search_results.html', {'results': results})
 
 #     return render(request, 'analysis.html')
-
-@csrf_exempt
-def get_influencer(request):
-    data = json.loads(request.body)
-    influencer_name= data.get('influencer_name')
-    influencer = testTable.objects.get(name=influencer_name)
-    influencer_data = {
-        'name': influencer.name,
-        'age': influencer.age,
-        'followerCount': influencer.followercount
-    }
-    return JsonResponse(influencer_data,safe=False)
