78c32108ce6aad2399f11492de8baf03868f6918
Wonhee0221 <totohwh3601@naver.com>
2023-08-18 17:49:20 +0900

    레이저차트

--- Cos_tard_web/static/templates/assets/js/chart.js
+++ Cos_tard_web/static/templates/assets/js/chart.js
@@ -3,7 +3,6 @@ $(function () {
    * -------
    * Data and config for chartjs
    */
-  'use strict';
   var data = {
     labels: ["2013", "2014", "2014", "2015", "2016", "2017"],
     datasets: [{
